051abadce6325e1f3fe0083747cc204698c3f234
Quentin LEMAIRE <lemairequentin22@gmail.com>
2019-02-04 10:18:33 +0100

    remove unecessary req

--- vizualize_data.py
+++ vizualize_data.py
@@ -1,9 +1,9 @@
 import matplotlib
 matplotlib.use('TkAgg')
 
-import smd.data.display as display
+import smd.data.display.events as display
 
-AUDIO_PATH = "/Users/quentin/Computer/DataSet/Music/speech_music_detection/ofai/audio_22050_600_100/332-RSR3-2012_02_01_10_45_01001.wav"
-ANNOTATION_PATH = "/Users/quentin/Computer/DataSet/Music/speech_music_detection/ofai/audio_22050_600_100/332-RSR3-2012_02_01_10_45_01001.txt"
+AUDIO_PATH = "./good_m.wav"
+ANNOTATION_PATH = "./good.txt"
 
 display.audio_with_events(AUDIO_PATH, ANNOTATION_PATH)
