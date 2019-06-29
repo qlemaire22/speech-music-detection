2d1fbaf4ab8dd8d49f60d9ad1d8aa5cfe585b27b
Quentin LEMAIRE <lemairequentin22@gmail.com>
2019-06-29 21:41:59 +0200

    bug fix

--- smd/data/postprocessing/threshold.py
+++ smd/data/postprocessing/threshold.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def apply_threshold(output, speech_threshold=0,5, music_threshold=0,5):
+def apply_threshold(output, speech_threshold=0.5, music_threshold=0.5):
     output[0] = np.where(output[0] >= speech_threshold, 1, 0)
     output[1] = np.where(output[1] >= music_threshold, 1, 0)
     return output.astype(int)
